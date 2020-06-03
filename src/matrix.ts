6f582196aa30e58708495ae9a3f055ab612d996d
phenomLi <1543046129@qq.com>
2020-06-03 16:13:40 +0800

    mat3

--- src/matrix.ts
+++ src/matrix.ts
@@ -11,7 +11,7 @@ export type Mat3 = Float32Array;
 export type Vec4 = Float32Array;
 export type Vec3 = Float32Array;
 export type Vec2 = Float32Array;
-export type Quat = number;
+export type Quat = Float32Array;
 
 export const _tempMatrix4: Mat4 = Matrix4.create();
 export const _tempMatrix3: Mat3 = Matrix3.create();
